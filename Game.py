bbc1f2fcdcb1e0445d66f165ac976b2de966c1d6
Nooha <noohakottangodan@gmail.com>
2023-03-31 06:19:29 +0530

    movements completed

--- Game.py
+++ Game.py
@@ -6,7 +6,10 @@ pygame.display.set_caption("Fortress Siege")
 x = y = 250
 radius = 15
 run = True
-vel = 10
+vel_x = 10
+vel_y = 10
+jump = False
+
 while run:
     win.fill((0, 0, 0))
 
@@ -14,14 +17,21 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    #movement        
     userInput = pygame.key.get_pressed()
-    if userInput[pygame.K_LEFT]:
-        x -= vel
-    if userInput[pygame.K_RIGHT]:
-        x += vel
-    if userInput[pygame.K_UP]:
-        y -= vel
-    if userInput[pygame.K_DOWN]:
-        y += vel
+    if userInput[pygame.K_LEFT] and x>0:
+        x -= vel_x
+    if userInput[pygame.K_RIGHT] and x<500-radius:
+        x += vel_x
+    if jump is False and userInput[pygame.K_SPACE]:
+        jump = True
+    if jump:
+        y -= vel_y   #since at top, vel must get 0
+        vel_y -= 1
+        if vel_y < -10:
+            jump = False
+            vel_y = 10
+        
     pygame.time.delay(10)
     pygame.display.update()
